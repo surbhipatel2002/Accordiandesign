315fd86b52a2b079b939bb84bc8d403a22f18d88
Akash Patel <akash.patel@vortilla.com>
2021-08-12 18:35:23 +0530

    add the text

---
+++ js/index.js
@@ -0,0 +1,33 @@
+function according(x) {
+    console.log(accord)
+    x.classList.toggle("click");
+    var first = document.getElementById("accord").classList;
+    var isclick = first.contains("click");
+    if (isclick) {
+        document.getElementById("paragraph's").style.display = "block";
+    } else {
+        document.getElementById("paragraph's").style.display = "none";
+    }
+}
+function according1(y) {
+    console.log(accord)
+    y.classList.toggle("clicks");
+    var first = document.getElementById("accord1").classList;
+    var isclick = first.contains("clicks");
+    if (isclick) {
+        document.getElementById("paragraph's1").style.display = "block";
+    } else {
+        document.getElementById("paragraph's1").style.display = "none";
+    }
+}
+function according2(z) {
+    console.log(accord)
+    z.classList.toggle("clicked");
+    var first = document.getElementById("accord2").classList;
+    var isclick = first.contains("clicked");
+    if (isclick) {
+        document.getElementById("paragraph's2").style.display = "block";
+    } else {
+        document.getElementById("paragraph's2").style.display = "none";
+    }
+}
